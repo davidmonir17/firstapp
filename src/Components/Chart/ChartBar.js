3d42100ccf06214e70ce0757332af4bf38cefc3f
david monir <davidmonir17@gmail.com>
2022-01-28 01:20:14 +0200

    spaces

--- src/Components/Chart/ChartBar.js
+++ src/Components/Chart/ChartBar.js
@@ -7,14 +7,14 @@ const ChartBar = (props) => {
     {
         barFilHieght=Math.round((props.value/props.MaxValue)*100)+'%'
     }
-  return  (
-  <div className='chart-bar '>
-      <div className='chart-bar__inner '>
-          <div className='chart-bar__fill' style={{height: barFilHieght}}></div>
-      </div>
-      <div className='chart-bar__label '>{props.label} </div>
-  </div>
-  )
+    return  (
+    <div className='chart-bar '>
+        <div className='chart-bar__inner '>
+            <div className='chart-bar__fill' style={{height: barFilHieght}}></div>
+        </div>
+        <div className='chart-bar__label '>{props.label} </div>
+    </div>
+)
 };
 
 export default ChartBar;
